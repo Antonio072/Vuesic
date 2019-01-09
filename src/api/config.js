e16f955654c98e8ccc7d32da2decbff2152affa6
Antonio072 <16030681@itcelaya.edu.mx>
2019-01-08 22:58:18 -0600

    First commit

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export default{
+    appname:'LinceBus',
+    apiKey: 'ded225e13cea3d48a93e781fc30439f8',
+    secret:  '4dfc9be1a681e458150e278e15fcdc25',
+    registeredTo:'arkantos_72'
+}
+
+
+
+// Application name 	LinceMusic
+// API key 	ded225e13cea3d48a93e781fc30439f8
+// Shared secret 	4dfc9be1a681e458150e278e15fcdc25
+// Registered to 	arkantos_72
